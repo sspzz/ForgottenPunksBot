a3287aa69acf1d7c44651063e4153c39a115ef09
sspzz <richiefilth@protonmail.com>
2022-10-09 17:07:45 +0200

    remove fetch, use request

--- commands/punk.js
+++ commands/punk.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const util = require('util');
+const request = require('request');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,9 +16,9 @@ module.exports = {
         "Invalid token, please use a value between 0 and 999."
       );
     }
-    const metaJSON = await fetch(
-      `https://forgottenpunks.wtf/api/meta/${token}`
-    ).then((response) => response.json());
+    const requestPromise = util.promisify(request);
+    const { body } = await requestPromise(`https://forgottenpunks.wtf/api/meta/${token}`);
+    const metaJSON = JSON.parse(body);
     const fields = metaJSON.attributes.map((a) => {
       return { name: a.trait_type, value: a.value, inline: true };
     });
