c5f40ff47afa11200ab2ba9ea98843de20a7d939
sspzz <richiefilth@protonmail.com>
2022-10-09 20:16:30 +0200

    min/max values for token parameter

--- commands/punk.js
+++ commands/punk.js
@@ -1,13 +1,17 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const util = require('util');
-const request = require('request');
+const util = require("util");
+const request = require("request");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("punk")
     .setDescription("Show a ForgottenPunk")
     .addIntegerOption((option) =>
-      option.setName("token").setDescription("Token ID of the ForgottenPunk")
+      option
+        .setName("token")
+        .setDescription("Token ID of the ForgottenPunk")
+        .setMinValue(0)
+        .setMaxValue(999)
     ),
   async execute(interaction) {
     const token = interaction.options.getInteger("token");
@@ -17,14 +21,18 @@ module.exports = {
       );
     }
     const requestPromise = util.promisify(request);
-    const { body } = await requestPromise(`https://forgottenpunks.wtf/api/meta/${token}`);
+    const { body } = await requestPromise(
+      `https://forgottenpunks.wtf/api/meta/${token}`
+    );
     const metaJSON = JSON.parse(body);
     const fields = metaJSON.attributes.map((a) => {
       return { name: a.trait_type, value: a.value, inline: true };
     });
     const embed = new EmbedBuilder()
       .setTitle(metaJSON.name)
-      .setURL(`https://opensea.io/assets/ethereum/0x4addac15971ab60ead954b8f15a67518730450e0/${token}`)
+      .setURL(
+        `https://opensea.io/assets/ethereum/0x4addac15971ab60ead954b8f15a67518730450e0/${token}`
+      )
       .setImage(`https://forgottenpunks.wtf/api/img/framed/${token}`)
       .addFields(fields);
     return interaction.reply({ embeds: [embed] });
