80babe13739cb36475c6be9ec847697ea4cd984c
sspzz <richiefilth@protonmail.com>
2022-10-11 01:09:19 +0200

    fix

--- commands/gif.js
+++ commands/gif.js
@@ -3,8 +3,9 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("gif")
-    .setDescription("Randomized GIF of ForgottenPunks");
+    .setDescription("Randomized GIF of ForgottenPunks"),
   async execute(interaction) {
+    await interaction.deferReply();
     const embed = new EmbedBuilder()
       .setImage("https://forgottenpunks.wtf/api/img/gif");
     return interaction.reply({ embeds: [embed] });
